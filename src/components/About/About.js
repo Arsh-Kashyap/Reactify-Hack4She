0efc29fca344d6b63b63451eafb119bdf8c9bdfc
Arsh Kashyap <arshk102001@gmail.com>
2020-11-21 17:17:50 +0530

    added about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,36 @@
 import React, { useContext } from "react";
+import { Container,Card,ListGroup, Row, Col } from "react-bootstrap";
+import classes from './About.module.css';
 import { userContext } from "../../context/userContext";
 
-const About = () => {
-	const { uid, name, amount, setUid, setName, setAmount } = useContext(
-		userContext
-	);
 
+const About = () => {
+
+	const images = ["https://i.postimg.cc/L5xXnCxQ/Stayfree-Spotlight.jpg","https://i.postimg.cc/hv1Cxxm5/2-Pix-Teller.png"];
+	const labels=["Stayfree","Whisper","Whisper"];
+	const sponsors = images.map((ele, index) => (
+		<Col lg={6} sm={12}>
+			<div className={[classes.card,classes.buzzoutonhover].join(" ")}>
+			<Card style={{width:"80%", height:"450px", margin:"auto"}}>
+					<div className={classes.image}>
+					<Card.Img style={{textAlign:"center"}} src={ele}/>
+					</div>
+					<Card.Header style={{fontSize:"25px"}}><strong>{labels[index]}</strong></Card.Header>
+					<ListGroup variant="flush">
+						<ListGroup.Item>Donated 100,000+ sanitary pads</ListGroup.Item>
+					</ListGroup>
+				</Card>
+			</div>
+		</Col>
+	));
 	return (
 		<div>
-			<h1>Hello</h1>
 
-			{name}
+			<Container>
+			<Row>
+				{sponsors}
+			</Row>
+			</Container>
 		</div>
 	);
 };
